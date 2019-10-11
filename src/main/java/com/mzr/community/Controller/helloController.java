d505a05bf3267d6a40c3c0cccf5917bd2ea79ce3
ryancurry-mz <hanma0426@163.com>
2019-10-11 15:30:13 +0800

    init repo

---
+++ src/main/java/com/mzr/community/Controller/helloController.java
@@ -0,0 +1,15 @@
+package com.mzr.community.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class helloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
